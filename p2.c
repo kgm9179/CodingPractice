0a7689f5d4cea5f460d0e13afca8ea7a869de727
KuGunmo <kgm9179@naver.com>
2021-04-29 10:35:51 +0900

    GitHub Test

---
+++ p2.c
@@ -0,0 +1,33 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+pthread_mutex_t m; // create Mutex m
+
+int g_count = 0; // Threads SHARE this resource
+
+
+void *thread_function(void * data){
+int i;
+char *thread_name = (char *)data;
+g_count = 0;
+for (i = 0; i<3; i++){
+
+	printf("%s COUNT %d\n", thread_name, g_count);
+	g_count++;
+	sleep(1);
+}
+
+}
+int main(){
+pthread_t testThread1, testThread2;
+int status;
+
+pthread_create(&testThread1,NULL,thread_function,(void *) "testThread1");
+pthread_create(&testThread2,NULL,thread_function,(void *) "testThread2");
+
+pthread_join(testThread1,(void *)&status);
+pthread_join(testThread2,(void *)&status);
+
+}
